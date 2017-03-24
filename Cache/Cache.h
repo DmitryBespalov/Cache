df3fa58c319f647a3337d4b355fe092354d91319
diamondsky <dima.bespalov@gmail.com>
2017-03-24 11:04:39 +0300

    First commit

---
+++ Cache/Cache.h
@@ -0,0 +1,19 @@
+//
+//  Cache.h
+//  Cache
+//
+//  Created by Dmitry Bespalov on 24/03/17.
+//  Copyright © 2017 Dmitry Bespalov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cache.
+FOUNDATION_EXPORT double CacheVersionNumber;
+
+//! Project version string for Cache.
+FOUNDATION_EXPORT const unsigned char CacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cache/PublicHeader.h>
+
+
