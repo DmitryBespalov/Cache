557a36524003a1686186fd19061bcbc834890c47
Dmitry Bespalov <dmitry.bespalov@zalando.de>
2017-04-07 10:45:38 +0200

    Added ARC implementation (no ghost lists yet)

--- Cache/ARCReplacementPolicy.swift
+++ Cache/ARCReplacementPolicy.swift
@@ -12,6 +12,66 @@ import Foundation
 ///
 /// Uses 4 LRU lists: 1 for recent entries, 1 for frequent entries, 1 for ghost list of 1st list, and 1 for ghost list
 /// of 2nd list.
-class ARCReplacmenetPolicy<KeyType>: ReplacementPolicy<KeyType> where KeyType: Hashable {
+class ARCReplacementPolicy<KeyType>: ReplacementPolicy<KeyType> where KeyType: Hashable {
+
+    var recentItems = PriorityQueue<KeyType>()
+    var frequentItems = PriorityQueue<KeyType>()
+    var age = 0
+    var maxRecentCost = 0
+    var maxFrequentCost = 0
+    var totalRecentCost = 0
+    var totalFrequentCost = 0
+    override var totalCost: Int {
+        get { return totalRecentCost + totalFrequentCost }
+        set { fatalError("Setting totalCost is prohibited") }
+    }
+
+    override init(maxCost: Int) {
+        maxFrequentCost = maxCost / 2
+        maxRecentCost = maxCost - maxFrequentCost
+        super.init(maxCost: maxCost)
+    }
+
+    override func evictedKeysForAdded(key newKey: KeyType, cost newCost: Int) -> [KeyType] {
+        var evicted = [KeyType]()
+        while totalRecentCost + newCost > maxRecentCost && !recentItems.isEmpty {
+            let item = recentItems.dequeue()
+            let cost = self.cost(of: item)
+            totalRecentCost -= cost
+            evicted.append(item)
+        }
+        while totalCost + newCost > maxCost && !frequentItems.isEmpty {
+            let item = frequentItems.dequeue()
+            let cost = self.cost(of: item)
+            totalFrequentCost -= cost
+            evicted.append(item)
+        }
+
+        totalRecentCost += newCost
+        costs[newKey] = newCost
+        recentItems.insert(newKey, priority: age)
+        age += 1
+        assert(totalCost <= maxCost)
+        return evicted
+    }
+
+    override func cacheHit(for key: KeyType) {
+        let grandTotal = totalCost
+        if let item = recentItems.remove(key) {
+            totalRecentCost -= cost(of: item)
+            while totalFrequentCost + cost(of: item) > maxFrequentCost && !frequentItems.isEmpty{
+                let (degraded, prio) = frequentItems.dequeueWithPriority()
+                totalFrequentCost -= cost(of: degraded)
+                recentItems.insert(degraded, priority: prio)
+                totalRecentCost += cost(of: degraded)
+            }
+            frequentItems.insert(item, priority: age)
+            totalFrequentCost += cost(of: item)
+        } else {
+            frequentItems.updatePriority(for: key, to: age)
+        }
+        age += 1
+        assert(totalCost == grandTotal, "Costs are messed up")
+    }
 
 }
---
+++ CacheTests/ARCReplacmenetPolicyTests.swift
@@ -0,0 +1,80 @@
+//
+//  ARCReplacmenetPolicyTests.swift
+//  Cache
+//
+//  Created by Dmitry Bespalov on 07/04/2017.
+//  Copyright © 2017 Dmitry Bespalov. All rights reserved.
+//
+
+import XCTest
+@testable import Cache
+
+class ARCReplacementPolicyTests: XCTestCase {
+
+    let policy = ARCReplacementPolicy<Int>(maxCost: 2)
+
+    func test_cacheNotFull_cacheHit_noEviction() {
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 0, cost: 1), [])
+        policy.cacheHit(for: 0)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 1, cost: 1), [])
+    }
+
+    func test_cacheFull_cacheHit_noEviction() {
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 0, cost: 1), [])
+        policy.cacheHit(for: 0)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 1, cost: 1), [])
+    }
+
+    func test_cacheFull_cacheMiss_evictsOne() {
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 0, cost: 1), [])
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 1, cost: 1), [0])
+    }
+
+    func test_whenCacheHit_staysInCache() {
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 0, cost: 1), [])
+        policy.cacheHit(for: 0)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 1, cost: 1), [])
+        policy.cacheHit(for: 1)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 2, cost: 1), [0])
+    }
+
+    func test_whenCacheHitToPreviouslyHit_updatesPriority() {
+        let policy = ARCReplacementPolicy<Int>(maxCost: 4)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 0, cost: 1), [])
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 1, cost: 1), [])
+        policy.cacheHit(for: 0)
+        policy.cacheHit(for: 1)
+        policy.cacheHit(for: 0)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 2, cost: 1), [])
+        policy.cacheHit(for: 2)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 3, cost: 1), [])
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 4, cost: 1), [1])
+    }
+
+    func test_evictionOfMultipleKeys() {
+        let policy = ARCReplacementPolicy<Int>(maxCost: 3)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 0, cost: 1), [])
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 1, cost: 1), [])
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 2, cost: 2).sorted(), [0, 1])
+    }
+
+    func test_replacementOfMultipleKeys() {
+        let policy = ARCReplacementPolicy<Int>(maxCost: 4)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 0, cost: 1), [])
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 1, cost: 1), [])
+        policy.cacheHit(for: 1)
+        policy.cacheHit(for: 0)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 2, cost: 2), [])
+        policy.cacheHit(for: 2)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 3, cost: 1), [1])
+    }
+
+    func test_evictionOfAllItems() {
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 0, cost: 1), [])
+        policy.cacheHit(for: 0)
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 1, cost: 1), [])
+        XCTAssertEqual(policy.evictedKeysForAdded(key: 2, cost: 2).sorted(), [0, 1])
+    }
+
+
+}
